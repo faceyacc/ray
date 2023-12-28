13a43ee84c9f9224278d4c0e707120ef1599da31
Ty <justfacey@gmail.com>
2023-12-28 13:53:05 -0500

    Added script to get dataflow from Bytewax.

---
+++ streaming_pipeline/run_real_time.py
@@ -0,0 +1,25 @@
+from streaming_pipeline import initialize
+from streaming_pipeline.flow import build as flow_builder
+
+def build_flow():
+    """
+    Buildss a Bytewax flow for real-time data processing.
+
+    Args:
+        env_file_path (str, optional): Path to env file. Defaults to ".env".
+        logging_config_path (str, optional): Path to the logging configuration file. Defaults to "logging.yml".
+        model_cache_dir (str, optional): Path to the directory where the model cache will be stored. Defaults to "None".
+        debug (bool, optional): Boolean flag to run the flow in debug mode. Defaults to "False".
+
+    Returns:
+        flow (perfect.Flow): The Bytewax flow for real-time data processing.
+    """
+
+    initialize(logging_config_path=logging_config_path, env_file_path=env_file_path)
+
+    flow = flow_builder(model_cache_dir=model_cache_dir, debug=debug)
+
+    return flow
+
+
+    
